8beb458398707c3e7f7776dfcfbd3474716be84d
Ankita <ankitarao01011998@gmail.com>
2020-12-19 22:09:58 -0400

    Welcome to Reactjs

--- 100-days-of-react/src/App.js
+++ 100-days-of-react/src/App.js
@@ -1,23 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import HelloReact from './components/day-1/HelloReact';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloReact />
     </div>
   );
 }
---
+++ 100-days-of-react/src/components/day-1/HelloReact.js
@@ -0,0 +1,14 @@
+/* 
+===================WELCOME TO THE WORLD OF REACT================================================================
+
+*/
+
+import React from 'react'
+
+export default function HelloReact() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
